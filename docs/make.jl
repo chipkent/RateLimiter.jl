6d7d993208cf54d4f9601e0988af0fc60651c2b5
David R. (Chip) Kent IV <chipkent@cecropiacapital.com>
2021-04-28 22:50:19 -0600

    Updated the documentation versions which are published.

--- docs/make.jl
+++ docs/make.jl
@@ -3,4 +3,9 @@ using Documenter, RateLimiter
 
 makedocs(sitename="RateLimiter.jl", modules = [RateLimiter])
 
-deploydocs(repo = "github.com/chipkent/RateLimiter.jl.git", devbranch = "main")
+deploydocs(
+    repo = "github.com/chipkent/RateLimiter.jl.git", 
+    devbranch = "main",
+    devurl = "main",
+    versions = ["stable" => "v^", "v#.#", "main" => "main"],
+)
